7093ab498deb09771392d98bdadef100bb21dde3
Naman Agrawal <a6.namanagrawal@gmail.com>
2022-02-01 20:19:31 +0530

    Movement Animation

--- app.js
+++ app.js
@@ -9,9 +9,42 @@ document.body.style.setProperty('--grid-size', gridSize);
 
 document.getElementById("container").innerHTML = Levels.map_to_html(Levels.levels[0].map)
 
-setInterval(() => {
+document.body.style.height = "" + window.innerHeight+ "px"
+document.body.style.wdith = "" + window.innerWidth + "px"
+canvas.style.width = "" + Math.min(window.innerHeight, window.innerWidth) + "px"
+canvas.style.height = "" + Math.min(window.innerHeight, window.innerWidth) + "px"
+
+window.onresize = () => {
     document.body.style.height = "" + window.innerHeight+ "px"
     document.body.style.wdith = "" + window.innerWidth + "px"
     canvas.style.width = "" + Math.min(window.innerHeight, window.innerWidth) + "px"
     canvas.style.height = "" + Math.min(window.innerHeight, window.innerWidth) + "px"
-}, 0)
+}
+
+function move(animation, x, y){
+    let player = document.getElementsByClassName("player")[0]
+    let coord = player.id.split(",")
+    player.style.animation = animation + " 0.1s alternate 2"
+    setTimeout(() => {
+        player.style.animation = ""
+        player.classList.remove("player")
+        document.getElementById("" + (parseInt(coord[0]) + x)+ "," + (parseInt(coord[1]) + y)).classList.add("player")
+    }, 100)
+}
+
+window.onkeydown = (event) => {
+    switch(event.key){
+        case "d":
+            move("moveRight", 1, 0)
+            break
+        case "a":
+            move("moveLeft", -1, 0)
+            break
+        case "w":
+            move("moveUp", 0, -1)
+            break
+        case "s":
+            move("moveDown", 0, 1)
+            break
+    }
+}
--- levels.js
+++ levels.js
@@ -15,30 +15,30 @@ export const Levels = {
 
     map_to_html: (level) => {
         let output = ""
-    
+        let lineNumber = 0
         level.forEach((line) => {
             output += '<div class="row">'
             for(let i = 0; i < line.length; i++){
-                console.log(line.charAt(i))
                 switch(line.charAt(i)){
                     case "#":
-                        output += '<div class="cell wall"></div>'
+                        output += '<div class="cell wall" id="' + i + "," + lineNumber + '"></div>'
                         break;
                     case "P":
-                        output += '<div class="cell player"></div>'
+                        output += '<div class="cell player" id="' + i + "," + lineNumber + '"></div>'
                         break;
                     case ".":
-                        output += '<div class="cell"></div>'
+                        output += '<div class="cell" id="' + i + "," + lineNumber + '"></div>'
                         break;
                     case "B":
-                        output += '<div class="cell box-red"></div>'
+                        output += '<div class="cell box-red" id="' + i + "," + lineNumber + '"></div>'
                         break;
                     case "G":
-                        output += '<div class="cell goal"></div>'
+                        output += '<div class="cell goal" id="' + i + "," + lineNumber + '"></div>'
                         break;
                 }
             }
             output += "</div>"
+            lineNumber++
         });
     
         return output
